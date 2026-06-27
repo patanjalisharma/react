8f6612845025624484cf9e2b83def7b22fc42261
patanjalisharma <patanjali.sharma03@gmail.com>
2026-06-27 23:07:46 +0530

    learn about useEffect

--- api/src/App.tsx
+++ api/src/App.tsx
@@ -1,17 +1,35 @@
 import axios from "axios";
+import { useEffect, useState } from "react";
 function App() {
-  async function getData() {
+  //async function getData() {
     // const res = await fetch("https://jsonplaceholder.typicode.com/users");
     // const data = await res.json()
     // console.log(data)
-    const res = await axios.get('https://jsonplaceholder.typicode.com/users')
+    // const res = await axios.get('https://jsonplaceholder.typicode.com/users')
 
-    console.log(res.data)
+    // console.log(res.data)
 
+    
+  //}
+
+  const [num, setNum] = useState(0)
+  const [num2, setNum2] = useState(0)
+  useEffect(function(){
+    console.log("first")
+  }, [num])
+
+  function changeVal() {
+    setNum(num+1);
+  }
+  function changeVal2() {
+    setNum2(num2+1);
   }
   return (
     <div>
-      <button onClick={getData}>Get Data</button>
+      <h1>{num}</h1>
+      <button onClick={changeVal}>click</button>
+      <h2>{num2}</h2>
+      <button onClick={changeVal2}>click2</button>
     </div>
   );
 }
